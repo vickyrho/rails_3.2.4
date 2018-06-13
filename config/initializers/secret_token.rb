19f153fe512d5345fa2302d4e1730b7a58262ede
Vigneshwaran Dharmalingam <vigneshwaran2121997@gmail.com>
2018-06-13 11:41:39 +0530

    basic pages and  routes added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '34dadbf67c4dcc9f6dd8a7cd6b3ba26860cd9ea64ebe4142342883c3268a0b4354359641f2f719ff72cc278b52c0e1a23919f161d499cae7857ec9602c38ec79'
